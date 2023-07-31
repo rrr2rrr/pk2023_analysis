627a808a946bcc4b67cee083c5062ff05e4e9d3f
Eugene Chernyavsky <eugene@runacap.com>
2023-07-31 15:48:16 +0400

    update readme and columns order

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from dataclasses import asdict
 
 
 def run_main_task(podlinnik=True):
-    lnks = get_all_links()
-    target_data = scrape_abitu(lnks)
+    links = get_all_links()
+    target_data = scrape_abitu(links)
     target_data = [x for x in target_data if x['dir']['level_select'] not in ('Среднее профессиональное образование')]
     df_all_guys = get_all_guys2(target_data)
     df_aggr = aggregate_guys(df_all_guys)
@@ -19,7 +19,7 @@ def run_main_task(podlinnik=True):
     df_min_points = pd.DataFrame([dict(**asdict(x), min=x.get_min_points()) for x in listUniversityDirection.values()])
     del df_min_points['students']
     df_min_points.to_csv('export.csv')
-    print(df_min_points.to_markdown())
+    print(df_min_points[['name', 'min', 'places', 'place', 'form', 'pay', 'level', 'link']].to_markdown())
 
 
 # Press the green button in the gutter to run the script.
